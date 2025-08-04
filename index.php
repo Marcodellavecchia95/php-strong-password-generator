50f22acdfadbf6f50637fccbe26e963c44137615
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-04 13:13:17 +0200

    ad milestone 1

--- index.php
+++ index.php
@@ -26,10 +26,14 @@ function password_generator($length = 0){
     <title>PHP Password Generator</title>
 </head>
 <body>
-    <form action="">
-        <label id="length" name="length" for="">Lunghezza password</label>
-        <input type="number">
-      <?php echo password_generator(5)?>
+    <h1>Generatore Password Casuale</h1>
+   
+    <form>
+        <label  for="number">Lunghezza password: </label>
+        <input id="length" name="length"  type="number" min=1 max=10>
+     <button>Genera Password</button>
     </form>
+     <hr>
+    <h3>La tua Password è: <?php if (isset($_GET["length"])) echo password_generator($_GET["length"]);?></h3>
 </body>
 </html>
