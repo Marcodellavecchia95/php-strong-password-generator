25a3aaa73bc0a4a1dd9cc9817c1c6159fa29b0dc
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-04 13:16:57 +0200

    add milestone 2

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<?php 
-function password_generator($length = 0){
-    $uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
-    $numbers = "12345678910";
-    $symbols = "!@#$%^&*()";
-    $allChars= $uppercaseAlphabet . $lowercaseAlphabet .  $numbers . $symbols;
-    $randomPass="";
-
-    for($i=0; $i<$length; $i++){
-        $random = rand(0,strlen($allChars)-1);
-        $randomChar= substr($allChars, $random ,1);
-       $randomPass .= $randomChar ;
-    }
-    return $randomPass;
-
-}
-?>
+<?php include "functions.php" ?> 
 
 
 <!DOCTYPE html>
