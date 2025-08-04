d826d99183a34b9ff3f0e25f7194233450f45a98
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-04 13:03:37 +0200

    create random password function

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php 
+function password_generator($length = 0){
+    $uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    $numbers = "12345678910";
+    $symbols = "!@#$%^&*()";
+    $allChars= $uppercaseAlphabet . $lowercaseAlphabet .  $numbers . $symbols;
+    $randomPass="";
+
+    for($i=0; $i<$length; $i++){
+        $random = rand(0,strlen($allChars)-1);
+        $randomChar= substr($allChars, $random ,1);
+       $randomPass .= $randomChar ;
+    }
+    return $randomPass;
+
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +26,10 @@
     <title>PHP Password Generator</title>
 </head>
 <body>
-    
+    <form action="">
+        <label id="length" name="length" for="">Lunghezza password</label>
+        <input type="number">
+      <?php echo password_generator(5)?>
+    </form>
 </body>
 </html>
